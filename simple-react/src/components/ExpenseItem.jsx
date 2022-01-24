7d2508a57943fc6fa81281abfbee38056e20d065
Joshua <joshuap16.personal@gmail.com>
2022-01-24 19:47:01 +0800

    expression

--- simple-react/src/components/ExpenseItem.jsx
+++ simple-react/src/components/ExpenseItem.jsx
@@ -1,8 +1,29 @@
 const ExpenseItem = () => {
+  const Month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  /* Getting User Date */
+  const d = new Date();
+  const ExpenseDate = `${
+    Month[d.getMonth()]
+  } ${d.getDate()}, ${d.getFullYear()}`;
+
   return (
     <div className="container">
       <div className="expense-item">
-        <div className="expense-item_date">January 01, 2022</div>
+        <div className="expense-item_date">{ExpenseDate}</div>
         <div className="expense-item_description">
           <div>House Loan</div>
           <div className="expense-item_amount">100,000</div>
