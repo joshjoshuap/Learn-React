38460ea1b82a8e4eccbe69e145e8c58698dd3c5e
Joshua <joshuap16.personal@gmail.com>
2022-01-24 20:22:45 +0800

    components-props

--- simple-react/src/App.js
+++ simple-react/src/App.js
@@ -3,10 +3,41 @@ import Heading from "./components/Heading";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  let expenses = [
+    {
+      id: 1,
+      title: "Housing Loan",
+      amount: 200,
+      date: "January 1, 2022",
+    },
+    {
+      id: 2,
+      title: "Car Loan",
+      amount: 10,
+      date: "January 1, 2022",
+    },
+    {
+      id: 3,
+      title: "Tution",
+      amount: 1000,
+      date: "January 1, 2022",
+    },
+  ];
+
   return (
     <div>
       <Heading />
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
     </div>
   );
 }
--- simple-react/src/components/ExpenseItem.jsx
+++ simple-react/src/components/ExpenseItem.jsx
@@ -1,36 +1,36 @@
-const ExpenseItem = () => {
-  const Month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+function ExpenseItem(props) {
+  // const month = [
+  //   "January",
+  //   "February",
+  //   "March",
+  //   "April",
+  //   "May",
+  //   "June",
+  //   "July",
+  //   "August",
+  //   "September",
+  //   "October",
+  //   "November",
+  //   "December",
+  // ];
 
   /* Getting User Date */
-  const d = new Date();
-  const ExpenseDate = `${
-    Month[d.getMonth()]
-  } ${d.getDate()}, ${d.getFullYear()}`;
+  // const expenseDate = `
+  // ${month[props.date.getMonth()]}
+  // ${props.date.getDate()},
+  // ${props.date.getFullYear()}`;
 
   return (
     <div className="container">
       <div className="expense-item">
-        <div className="expense-item_date">{ExpenseDate}</div>
+        <div className="expense-item_date">{props.date}</div>
         <div className="expense-item_description">
-          <div>House Loan</div>
-          <div className="expense-item_amount">100,000</div>
+          <div>{props.title}</div>
+          <div className="expense-item_amount">{props.amount}</div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default ExpenseItem;
