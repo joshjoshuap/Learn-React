43bb70c820c433675ec845cb2600d57e34b8a515
Joshua <joshuap16.personal@gmail.com>
2022-01-24 21:26:05 +0800

    split into multiple components

--- simple-react/src/components/ExpenseItem.jsx
+++ simple-react/src/components/ExpenseItem.jsx
@@ -1,29 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
+
 function ExpenseItem(props) {
-  // const month = [
-  //   "January",
-  //   "February",
-  //   "March",
-  //   "April",
-  //   "May",
-  //   "June",
-  //   "July",
-  //   "August",
-  //   "September",
-  //   "October",
-  //   "November",
-  //   "December",
-  // ];
-
-  /* Getting User Date */
-  // const expenseDate = `
-  // ${month[props.date.getMonth()]}
-  // ${props.date.getDate()},
-  // ${props.date.getFullYear()}`;
-
   return (
     <div className="container">
       <div className="expense-item">
-        <div className="expense-item_date">{props.date}</div>
+        <ExpenseDate date={props.date} />
         <div className="expense-item_description">
           <div>{props.title}</div>
           <div className="expense-item_amount">{props.amount}</div>
---
+++ simple-react/src/components/Expenses.js
@@ -0,0 +1,23 @@
+  let Expenses = [
+    {
+      id: 1,
+      title: "Housing Loan",
+      amount: 200,
+      date: new Date(),
+    },
+    {
+      id: 2,
+      title: "Car Loan",
+      amount: 10,
+      date: new Date(),
+    },
+    {
+      id: 3,
+      title: "Tuition",
+      amount: 1000,
+      date: new Date(),
+    },
+  ];
+
+  export default Expenses;
+
