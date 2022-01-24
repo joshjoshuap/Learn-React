6835cece849c82b0b4c4dd2350cccad87b7c73c4
Joshua <joshuap16.personal@gmail.com>
2022-01-24 19:31:23 +0800

    add css styling

--- simple-react/src/components/ExpenseItem.jsx
+++ simple-react/src/components/ExpenseItem.jsx
@@ -1,10 +1,12 @@
 const ExpenseItem = () => {
   return (
-    <div>
-      <div>Januarty 01, 2022</div>
-      <div>
-        <div>House Loan</div>
-        <div>100,000</div>
+    <div className="container">
+      <div className="expense-item">
+        <div className="expense-item_date">January 01, 2022</div>
+        <div className="expense-item_description">
+          <div>House Loan</div>
+          <div className="expense-item_amount">100,000</div>
+        </div>
       </div>
     </div>
   );
