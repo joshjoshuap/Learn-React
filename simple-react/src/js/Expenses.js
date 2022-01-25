a697bca13431395f06c6b2e099d2052f853b6ff4
Joshua <joshuap16.personal@gmail.com>
2022-01-25 19:02:39 +0800

    refractor

--- simple-react/src/js/Expenses.js
+++ simple-react/src/js/Expenses.js
@@ -1,23 +1,22 @@
-  let Expenses = [
-    {
-      id: 1,
-      title: "Housing Loan",
-      amount: 200,
-      date: new Date(),
-    },
-    {
-      id: 2,
-      title: "Car Loan",
-      amount: 10,
-      date: new Date(),
-    },
-    {
-      id: 3,
-      title: "Tuition",
-      amount: 1000,
-      date: new Date(),
-    },
-  ];
+let Expenses = [
+  {
+    id: "1",
+    title: "Housing Loan",
+    amount: 200,
+    date: new Date(2021, 1, 1),
+  },
+  {
+    id: "2",
+    title: "Car Loan",
+    amount: 10,
+    date: new Date(2021, 1, 2),
+  },
+  {
+    id: "3",
+    title: "Tuition",
+    amount: 1000,
+    date: new Date(2021, 1, 3),
+  },
+];
 
-  export default Expenses;
-
+export default Expenses;
