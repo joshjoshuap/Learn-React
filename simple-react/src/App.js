5695ee0af3e892094cf2678a7f111bf75324a5e8
Joshua <joshuap16.personal@gmail.com>
2022-01-26 18:20:23 +0800

    fix adding expense

--- simple-react/src/App.js
+++ simple-react/src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./js/Expenses"; // importing data
 import Expense from "./components/Expense/Expense";
 import NewExpense from "./components/Expense/NewExpense";
 import Heading from "./components/UI/Heading";
 
 const App = () => {
+  const [expenses, setExpenses] = useState(Expenses);
   const addExpense = (expense) => {
-    console.log(expense);
+    // prevExpense = Expenses or expenses
+    setExpenses((prevExpense) => {
+      console.log([expense, ...prevExpense]);
+      return [expense, ...prevExpense];
+    }); // add new created expense with existed expenses to new array set to expenses
   };
 
   return (
@@ -15,7 +20,7 @@ const App = () => {
       {/* Get the value of addingExpense from saveExpenseData() in NewExpense.jsx*/}
       <NewExpense addingExpense={addExpense} />
       {/* Pass the value of addingExpense to addExpense()*/}
-      <Expense item={Expenses} />
+      <Expense item={expenses} />
     </div>
   );
 };
