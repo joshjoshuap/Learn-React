92f9ea340681854338cc3df8698a59c32ae18808
Joshua <joshuap16.personal@gmail.com>
2022-02-09 16:47:29 +0800

    delete

--- Basics, Fundamentals/Components - Mapping and Looping Components/src/components/details.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// props will retrieve data from app.jsx <Details /> attributes
-function Details(props) {
-  return <p className="info">{props.detailInfo}</p>;
-}
-
-export default Details;
--- Basics, Fundamentals/Props - Passing Data Mutiple Components/src/components/details.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// retrieve data from card.jsx <Details detailinfo/>
-function Details(props) {
-  return <p className="info">{props.detailInfo}</p>;
-}
-
-export default Details;
