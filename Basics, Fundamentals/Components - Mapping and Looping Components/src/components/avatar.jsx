cc0f6b61ced495a98cc8b39cd4058705848146a3
Joshua <joshuap16.personal@gmail.com>
2022-02-27 19:28:04 +0800

    organize

--- Basics, Fundamentals/Components - Mapping and Looping Components/src/components/avatar.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-// retrieve data from card.jsx <Avatar img/>
-function Avatar(props) {
-  return <img src={props.img} alt="avatar_img" className="circle-img" />;
-}
-
-export default Avatar;
