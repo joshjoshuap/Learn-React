567a34b55b66f4ced5dc4c8951d303f7fc062473
Joshua <joshuap16.personal@gmail.com>
2022-03-01 18:42:21 +0800

    learn styling

---
+++ styling-basic/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+  <div>
+    <div class="container">
+      <h1> This is Heading h1 </h1>
+      <h2> This is Heading h2 </h2>
+      <h3> This is Heading h3 </h3>
+      <h4> This is Heading h4 </h4>
+      <h5> This is Heading h5 </h5>
+      <h6> This is Heading h6 </h6> 
+    </div>
+
+    <div class="container"> 
+     <p> 
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et arcu ac mauris ullamcorper tempor. Phasellus blandit ac eros vitae eleifend. In molestie, felis et porttitor egestas, ipsum eros suscipit turpis, id laoreet velit lectus et urna. Nulla fringilla nunc id libero sagittis, sed mattis felis molestie. Curabitur massa tortor, lobortis id porta eget, molestie eget quam. Donec sed rhoncus quam. Suspendisse mattis lacinia lacus, eget pellentesque tellus. Sed eget tempus lectus. Aenean in pellentesque odio, in rutrum justo. Duis eget purus vel nunc fermentum tempor. Vivamus eget quam non neque interdum fringilla nec in justo. Nunc varius tortor ipsum. Sed et hendrerit libero. Nullam consectetur purus eu tortor porttitor commodo.
+     </p>
+    </div>
+  </div>
+  );
+}
+
+export default App;
