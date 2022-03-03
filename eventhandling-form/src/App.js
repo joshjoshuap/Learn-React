95e061644536c34724a066850f39581f3ccc6b1f
Joshua <joshuap16.personal@gmail.com>
2022-03-03 20:45:37 +0800

    learn event

--- eventhandling-form/src/App.js
+++ eventhandling-form/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [inputName, setInput] = useState("");
+  const [name, setName] = useState("");
+
+  let getValue = (event) => {
+    setInput(event.target.value);
+  }
+
+  let changeName = (event) => {
+    setName(inputName);
+
+    event.preventDefault();
+  }
+
   return (
-    <div>
-      <h1>Test</h1>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <form onClick={changeName}>
+        <input onChange={getValue} type="text" placeholder="What is your name" value={inputName}/>
+        <button type="Submit">Submit</button>
+      </form>
     </div>
   );
 }
