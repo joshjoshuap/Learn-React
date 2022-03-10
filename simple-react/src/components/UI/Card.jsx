3686a00b02159c48d3007b0979be9228cb1e72b2
Joshua <joshuap16.personal@gmail.com>
2022-03-10 17:28:35 +0800

    fix

--- simple-react/src/components/UI/Card.jsx
+++
@@ -1,6 +0,0 @@
-const Card = (props) => {
-  const classes = `card ${props.className}`; // calling existed classname and add card
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default Card;
