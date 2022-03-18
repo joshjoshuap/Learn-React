3375cac47831216642cd48d739d8c862da81ab66
Joshua <joshuap16.personal@gmail.com>
2022-03-18 21:02:02 +0800

    delete files

--- sample-app-2/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import style from "./Card.module.css";
-
-const Card = (props) => {
-  return (
-    <div className={`${style.Card} ${props.className}`}>{props.children /* This is the content inside Card */ }</div>
-  );
-};
-
-export default Card;
