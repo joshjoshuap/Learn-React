da4d136787da69882b03e05cf905ca919e27da3a
Joshua <joshuap16.personal@gmail.com>
2022-03-26 20:03:28 +0800

    authentication

---
+++ sample-app-authentication_login-logout/src/components/Layout/MainNavigation.js
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import classes from './MainNavigation.module.css';
+import AuthContext from '../../store/auth-context';
+
+const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  return (
+    <header className={classes.header}>
+      <Link to="/">
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/auth">Login</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
